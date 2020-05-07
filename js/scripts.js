ef95b4afd822c3771c2f93e48b4c99d13b86e1a2
CharleenCBute <64311921+CharleenCBute@users.noreply.github.com>
2020-05-07 23:41:16 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function Zebras() {
+
+	var style = document.createElement('style')
+	document.head.appendChild(style);
+	style.sheet.insertRule('tr:nth-child(odd) {background-color:darkgray;}');
+
+	
+}
